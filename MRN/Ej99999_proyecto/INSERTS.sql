7fc081d7dc5c34c5d3e67c833a7072ca6449654e
Fraile2 <145047146+Fraile2@users.noreply.github.com>
2024-01-10 20:24:34 +0100

    Inserts

---
+++ MRN/Ej99999_proyecto/INSERTS.sql
@@ -0,0 +1,330 @@
+INSERT INTO persona(dni, n_pasaporte, nombre, apellidos, fecha_nacimiento) -- Alejandro Fraile del Olmo
+VALUES('44985796G','44985796G','alejandro','fraile del olmo', '2003-03-05'), -- cliente
+('86873434L','86873434L','jesus','valenzuela santana','2004-04-12'), -- cliente
+('23188871H','23188871H','jessica maritza','lalaleo guangasi','2002-02-11'), -- cliente
+('48006290T','48006290T','angel','almache cuadrado','2004-04-24'), -- cliente
+('73860331V','73860331V','carlos','villarroel gonzalez','1983-03-01'), -- clientes
+('52955438P','52955438P','victoria','estrada alicante','2003-11-02'), -- clientes
+('90095416T','90095416T','victoria','lopez canfran','1998-03-11'), -- clientes
+('68649360H','68649360H','pepito','grillo xdniano','1975-07-08'), -- clientes
+('63318845Y','63318845Y','agustin','aguilera martin','1986-02-10'), -- clientes
+('92860136M','92860136M','clara','campoamor galicia','1989-04-09'), -- clientes
+('93690258B','93690258B','angel','zamora sanchez','2000-12-05'), -- clientes
+('51807164D','51807164D','helen','chufe de valencia','1976-10-21'), -- clientes
+('27715253T','27715253T','leonardo','di caprio','1997-05-20'), -- clientes
+('50832138E','50832138E','aitor','tilla menganito','1967-10-30'), -- clientes
+('25579646N','25579646N','lidia','ruiz martinez','2001-02-09'), -- clientes
+('29410835E','29410835E','gonzalo','lozano palomo','1967-04-18'), -- clientes
+('69131212C','69131212C','alex','lozano martin','2000-01-01'), -- clientes
+('16917725Y','16917725Y','sergio','ramos arguelles','2001-12-12'), -- clientes
+('42799486M','42799486M','jose','cela olmos','1965-09-07'), -- clientes
+('30715198D','30715198D','estrella','de la muerte','1998-12-17'), -- clientes
+('86654559N','86654559N','angel','caido turin','1992-08-19'), -- clientes
+('75618098D','75618098D','erling','braut haaland','2000-07-21'), -- clientes
+('66592600X','66592600X','jose luis','de leon','1999-03-03'), -- clientes
+('17778073Q','17778073Q','lola','martinez camacho','1980-09-13'), -- clientes
+('72636918C','72636918C','iker','casillas fernandez','1981-05-20'), -- clientes
+('77152963S','77152963S','jorge','campos navarrete','1966-10-15'), -- clientes
+('80970054A','80970054A','felix','sanchez martin','2004-03-17'), -- clientes
+('79003488Y','79003488Y','ana maria','zamora del olmo','2002-01-19'), -- clientes
+('33360699G','33360699G','maria jose','zambrano fernandez','2002-10-19'), -- clientes
+('69125738C','69125738C','alfonso','fraile del olmo','1998-03-31'),-- clientes
+('84936356R','84936356R','alfonso','fraile sanchez','1964-10-20'), -- viajeros
+('96065902P','96065902P','eugenio','da silva','1980-02-01'), -- viajeros
+('90539327B','90539327B','roberto jose','leal guillen','1979-06-28'), -- viajeros
+('23639747W','23639747W','jezabel','perez martin','1977-04-16'), -- viajeros
+('51454163N','51454163N','juan','del sol','1976-01-24'), -- viajeros
+('87442081Z','87442081Z','saul','sanchez da silva','1987-10-02'), -- viajeros
+('27813318Q','27813318Q','jose juan','lopez canfran','1986-07-04'), -- viajeros
+('35371322J','35371322J','jose juan','macias prada','1990-01-31'), -- viajeros
+('24507959X','24507959X','alejandro','olmos pereda','2002-08-16'), -- viajeros
+('10499607S','10499607S','alejandro','del olmo cerrada','1965-06-27'), -- viajeros
+('32768983D','32768983D','leonardo','da vinci','1976-04-06'), -- viajeros
+('97393760F','97393760F','estrella','villaroel zamora','2000-01-09'), -- viajeros
+('76120144B','76120144B','luz camila','fernandez sanchez','2002-10-12'), -- viajeros
+('23242379M','23242379M','kilyan','mbappe','1970-02-15'), -- viajeros
+('10938076N','10938076N','marcela','sanchez canfran','2001-06-10'), -- viajeros
+('88132520Q','88132520Q','marcelo','santana olmos','1976-12-13'), -- viajeros
+('78673028X','78673028X','bartolome','cosito ferran','1967-11-23'), -- viajeros
+('86191694E','86191694E','jesus','seisdedos fernandez','1980-09-18'), -- viajeros
+('43979761N','43979761N','lucia','fraile canfran','1975-02-01'), -- viajeros
+('28930562N','28930562N','lucio','reverte olmos','2001-03-23'), -- viajeros
+('28894815F','28894815F','sergio','ramos reverte','1999-01-09'), -- viajeros
+('18864924W','18864924W','alejandro','del olmo fraile','1987-05-03'), -- viajeros
+('81295686R','81295686R','oscar','mefferts frutos','1998-03-31'), -- viajeros
+('45777194H','45777194H','alegria','guangasi quevedo','1997-09-21'), -- viajeros
+('85665738F','85665738F','alejandro','tolome perez','1999-09-19'), -- viajeros
+('93219482E','93219482E','alejandro','da silva','1998-10-12'), -- viajeros
+('68120808Y','68120808Y','alejandro','reverte canfran','1998-10-18'), -- viajeros
+('39355567Z','39355567Z','alegria','mefferts macias','1994-07-09'), -- viajeros
+('36915547H','36915547H','alejandro jose','lopez seisdedos','2002-02-05'), -- viajeros
+('34507510N','34507510N','alejandro','del olmo ferraz','1993-09-12'); -- viajeros
+
+INSERT INTO cliente(dni_cliente) -- Alejandro Fraile del Olmo
+VALUES('44985796G'), 
+('86873434L'), 
+('23188871H'), 
+('48006290T'), 
+('73860331V'), 
+('52955438P'), 
+('90095416T'), 
+('68649360H'), 
+('63318845Y'), 
+('92860136M'), 
+('93690258B'), 
+('51807164D'), 
+('27715253T'),
+('50832138E'),
+('25579646N'),
+('29410835E'),
+('69131212C'),
+('16917725Y'),
+('42799486M'),
+('30715198D'),
+('86654559N'),
+('75618098D'),
+('66592600X'),
+('17778073Q'),
+('72636918C'), 
+('77152963S'), 
+('80970054A'), 
+('79003488Y'), 
+('33360699G'), 
+('69125738C');
+
+INSERT INTO avion (matricula, marca, modelo) VALUES ("EC", "Boeing", "Boeing 737-100")
+INSERT INTO avion (matricula, marca, modelo) VALUES ("EC", "Boeing", "Boeing 737-100");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("YA", "Airbus", "Airbus A320");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("ZA", "Embraer", "E-Jet");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("D", "Airbus", "Airbus A380");
+INSERT INTO avion (matricula, marca, modelo) VALUES("7T", "Airbus", "Airbus A318");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("C3", "Airbus", "Airbus A319");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("D2", "Airbus", "Airbus A320");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("VP-A", "Airbus", "Airbus A321");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("V2", "Airbus", "Airbus A330-200");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("F-OG", "Airbus", "Airbus A330-300");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("PJ", "Airbus", "Airbus A350-900"); 
+INSERT INTO avion (matricula, marca, modelo) VALUES ("HZ", "Airbus", "Airbus A350-1000"); 
+INSERT INTO avion (matricula, marca, modelo) VALUES ("LQ", "Airbus", "Airbus A380");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("EK", "Airbus", "Airbus A300");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("P4", "Airbus", "Airbus A310");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("VH", "Boeing", "Boeing 737-200");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("OE", "Boeing", "Boeing 737-300"); 
+INSERT INTO avion (matricula, marca, modelo) VALUES ("4K", "Boeing", "Boeing 737-400"); 
+INSERT INTO avion (matricula, marca, modelo) VALUES ("C6", "Boeing", "Boeing 737-500");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("A9C", "Boeing", "Boeing 737-600");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("S2", "Boeing", "Boeing 737-700");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("8P", "Boeing", "Boeing 737-800"); 
+INSERT INTO avion (matricula, marca, modelo) VALUES ("OO", "Boeing", "Boeing 737-900");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("V3", "Boeing", "Boeing 737 MAX 7"); 
+INSERT INTO avion (matricula, marca, modelo) VALUES ("TY", "Boeing", "Boeing 737 MAX 8");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("VP-B", "Boeing", "Boeing 737 MAX 9");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("A5", "Boeing", "Boeing 737 MAX 10");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("EW", "Boeing", "Boeing 737-100");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("CP", "Embraer", "ERJ 135");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("T9", "Embraer", "Embraer EMB 110 Bandeirante");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("A2", "Embraer", "Embraer EMB 120 Brasilia");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("PP", "Embraer", "Embraer E-Jet E170");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("V8", "Embraer", "Embraer E-Jet E175"); 
+INSERT INTO avion (matricula, marca, modelo) VALUES ("LZ", "Embraer", "Embraer E-Jet E190");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("XT", "Embraer", "Embraer E-Jet E195"); 
+INSERT INTO avion (matricula, marca, modelo) VALUES ("9U", "Embraer", "Embraer EMB 145");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("XU", "Embraer", "Embraer Praetor 500");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("TJ", "Embraer", "Embraer Praetor 600");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("C", "Boeing", "Boeing 747-200");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("D4", "Boeing", "Boeing 747-300"); 
+INSERT INTO avion (matricula, marca, modelo) VALUES ("TT", "Boeing", "Boeing 747-400");
+INSERT INTO avion (matricula, marca, modelo) VALUES ("CC", "Boeing", "Boeing 747-8");
+
+INSERT INTO billete(fecha, numero_asiento, coste, tipo)
+VALUES('2000-12-23','15AF',120.96,'ida'), -- Angel Almache Cuadrado
+('2015-04-30','98BA',1800.95,'ida_vuelta'),
+('2004-01-27','55AR',1200.98,'ida'),
+('2002-08-12','90AV',240.96,'ida'),
+('2018-11-09','71AS',2400.86,'ida_vuelta'),
+('2009-07-20','82AB',3000.89,'ida_vuelta'),
+('2008-12-05','29FB',100.99,'ida'),
+('2011-06-18','42DA',1000.98,'ida'),
+
+('2002-12-12','31A8', 930.32 ,'ida_vuelta'),-- Jesus Valenzuela Santana
+('2002-12-12','31A9', 930.32 ,'ida_vuelta'),
+('2015-08-21','A412', 880.01 ,'ida'),
+('2022-07-09','A413', 550.60 ,'ida'),
+('2019-12-04','31B0', 749.03 ,'ida_vuelta'),
+('2017-05-17','A412', 120.50 ,'ida_vuelta'),
+('2014-09-30','E101', 140.90 ,'ida'),
+('2011-10-14','E111', 240.10 ,'ida_vuelta'),
+
+('2009-11-12','31A8', 930.32 ,'ida_vuelta'),-- Alejandro Fraile del Olmo
+('2009-11-12','31A9', 930.32 ,'ida_vuelta'),
+('2020-06-10','A412', 880.01 ,'ida'),
+('2020-06-12','A413', 550.60 ,'ida'),
+('2021-08-19','31B0', 749.03 ,'ida_vuelta'),
+('2017-08-22','A412', 120.50 ,'ida_vuelta'),
+('2019-09-23','E101', 140.90 ,'ida'),
+('2016-03-24','E111', 240.10 ,'ida_vuelta'),
+
+('2010-11-13','31A8', 300.32 ,'ida_vuelta'), -- Jessica Maritza Lalaleo
+('2011-11-12','31A9', 300.32 ,'ida_vuelta'),
+('2021-06-10','A412', 880.01 ,'ida'),
+('2020-06-12','A413', 550.60 ,'ida'),
+('2021-08-19','31B0', 801.03 ,'ida_vuelta'),
+('2021-08-22','A412', 129.50 ,'ida_vuelta'),
+('2020-06-10','E101', 142.90 ,'ida'),
+('2016-03-24','E111', 242.10 ,'ida_vuelta');
+
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2013-04-22 07:30", 8.15); -- Jesus Valenzuela Santana
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2013-04-22 08:00:00", 7.30);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2013-04-21 08:00:00", 8.40);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2013-04-23 08:15:00", 8.10);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2013-04-24 08:15:00", 6.00);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2013-04-25 08:20:00", 7.00);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2013-04-26 08:25:00", 7.00);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2013-04-27 08.30.00", 2.30);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2013-04-28 08:45:00", 4.00);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2013-04-29 08:50:00", 6.90);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2023-01-07 09:00:00", 1.00);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2022-02-12 09:02:00", 4.00);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2010-07-15 09:05:10", 7.3);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2011-08-30 09:10:00", 3.2);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2012-02-03 09:15:00", 2.6);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2011-07-08 09.30:00", 5.00);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2008-06-13 10.00:00", 6.00);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2007-07-14 10.05:00", 8.5);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2003-09-17 10.10:00", 16);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2007-05-23 10.15:00", 10);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2007-06-17 10.20:00", 11.6);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2022-08-26 10:25:30", 14);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2022-09-12 10:30:00", 3.2);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2021-04-13 10:35:30", 2.10);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2020-02-17 10:40:00", 4.5);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2024-05-14 10:45:00", 8);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2013-04-25 17:15:00", 13);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2012-09-18 18.00:00", 7);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2009-03-01 18:04:00", 6.30);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2009-03-02 18:13:40", 7);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2007-06-28 18:30:00", 5);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2006-06-28 19:00:07", 4);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2002-11-15 14:00:00", 2);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2006-06-22 16:28:00", 3.30);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2009-05-27 16:30:00", 4);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2010-09-04 16:35:00", 6);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2011-02-14 09:00:00", 2);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2014-05-23 10:00:00", 21);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2017-09-24 21:05:00", 9);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2014-05-09 21:40:00", 6);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2012-09-21 21:45:00", 4);
+INSERT INTO vuelo (fecha_hora_vuelo, duracion) VALUES ("2023-03-07 22:00:00", 6);
+
+INSERT INTO viajero(dni_viajero, billete, vuelo)
+VALUES('84936356R', 1, 1),
+('96065902P', 2, 2),
+('90539327B', 3, 6),
+('23639747W', 4, 4),
+('51454163N', 5, 4),
+('87442081Z', 2, 2),
+('27813318Q', 6, 8),
+('35371322J', 7, 7),
+('24507959X', 8, 8),
+('10499607S', 7, 7),
+('32768983D', 1, 1),
+('97393760F', 9, 9),
+('76120144B', 10, 10),
+('23242379M', 10, 10),
+('10938076N', 11, 11),
+('88132520Q', 13, 13),
+('78673028X', 12, 12),
+('86191694E', 14, 14),
+('43979761N', 15, 15),
+('28930562N', 8, 8),
+('28894815F', 15, 15),
+('18864924W', 12, 12),
+('81295686R', 11, 11),
+('45777194H', 10, 10),
+('85665738F', 9, 9),
+('93219482E', 9, 9),
+('68120808Y', 6, 6),
+('39355567Z', 6, 6),
+('36915547H', 5, 5),
+('34507510N', 5, 5);
+
+INSERT INTO terminal (nombre) values ("Terminal1"); -- ANGEL
+INSERT INTO terminal (nombre) values ("Terminal2");
+INSERT INTO terminal (nombre) values ("Terminal3");
+INSERT INTO terminal (nombre) values ("Terminal4");
+INSERT INTO terminal (nombre) values ("Terminal5");
+INSERT INTO terminal (nombre) values ("Terminal6");
+INSERT INTO terminal (nombre) values ("Terminal7");
+INSERT INTO terminal (nombre) values ("Terminal8");
+INSERT INTO terminal (nombre) values ("Terminal9");
+INSERT INTO terminal (nombre) values ("Terminal10");
+
+INSERT INTO puerta (terminal,vuelo) values (1,34); -- ANGEL
+INSERT INTO puerta (terminal,vuelo) values (10,4);
+INSERT INTO puerta (terminal,vuelo) values (3,3);
+INSERT INTO puerta (terminal,vuelo) values (5,23);
+INSERT INTO puerta (terminal,vuelo) values (1,21);
+INSERT INTO puerta (terminal,vuelo) values (9,4);
+INSERT INTO puerta (terminal,vuelo) values (6,14);
+INSERT INTO puerta (terminal,vuelo) values (7,13);
+INSERT INTO puerta (terminal,vuelo) values (2,39);
+INSERT INTO puerta (terminal,vuelo) values (8,25);
+INSERT INTO puerta (terminal,vuelo) values (10,40);
+INSERT INTO puerta (terminal,vuelo) values (3,20);
+INSERT INTO puerta (terminal,vuelo) values (6,5);
+INSERT INTO puerta (terminal,vuelo) values (10,9);
+INSERT INTO puerta (terminal,vuelo) values (2,33);
+
+INSERT INTO aerolinea (nombre) VALUES ("Iberia");
+INSERT INTO aerolinea (nombre) VALUES ("Vueling");
+INSERT INTO aerolinea (nombre) VALUES ("Eazy Jet");
+INSERT INTO aerolinea (nombre) VALUES ("Air Europa");
+INSERT INTO aerolinea (nombre) VALUES ("Sky Airline");
+INSERT INTO aerolinea (nombre) VALUES ("Air Frnace");
+INSERT INTO aerolinea (nombre) VALUES ("America Airline");
+INSERT INTO aerolinea (nombre) VALUES ("Delta Air Lines");
+INSERT INTO aerolinea (nombre) VALUES ("Emirates Airline");
+
+INSERT INTO piloto(num_galones, antiguedad, nombre, apellidos, aerolinea, id_copiloto)
+VALUES
+(1, "2012-01-01", "Cristian", "Florico Vilcu", 3,1),
+(2, "2013-02-15", "Marcos", "Gutierrez Planas", 2,1),
+(2, "2010-04-14", "Francisco", "Fernandez Brañas", 3,1),
+(2, "2017-01-07", "Pilar", "Sanguino Ampuero", 4, 2),
+(3, "2016-02-15", "Carlos", "Sanchez Puero", 5, 2),
+(3, "2017-09-15", "Jose", "Moreno Sanguino", 5, 3),
+(1, "2010-09-01", "Martin", "Pinto Castillo", 6, 3),
+(3, "2019-01-04", "Felix", "Izquierdo Bejarano", 5, 4),
+(3, "2012-11-15", "Brian", "Novillo Gracia", 4, 4),
+(3, "2014-02-18", "Oliver", "Delgado Montejo", 4, 5),
+(3, "2014-02-18", "Mario", "Rimon Ruiz", 1, 5),
+(3, "2014-02-18", "Antonio", "Colmenarejo Delgado", 1, 6),
+(1, "2013-10-03", "Franco", "Heredia Navarro", 5, 6),
+(4, "2013-10-13", "Manuel", "Peña Gonzalez", 6, 7),
+(4, "2013-10-30", "Sergio", "Ramos Garcia", 2, 7),
+(4, "2013-10-30", "Roman", "Esteban Romanillos", 5, 8),
+(4, "2015-01-12", "Mateo", "Moreno Fernandez", 3, 8),
+(2, "2012-10-01", "Marco", "Martin Granda", 2, 9),
+(2, "2012-01-01", "Alex", "Ferrandez Cacho", 3, 10),
+(2, "2012-01-19", "Carlos", "Sanchez Ramirez", 4, 10),
+(4, "2018-12-10", "Manuel", "Mingo Sanchez", 4, 11),
+(3, "2018-12-10", "Victor", "Hernando Jimenez", 3, 11),
+(3, "2018-12-10", "Israel", "Vega dominguez", 5, 12),
+(4, "2014-02-25", "Ismael", "Fresno Fernandez", 4, 12),
+(3, "2014-02-25", "Ariel", "Garcia Cabrera", 1, 13),
+(3, "2014-02-25", "Angel", "Gallego Largo", 4, 13),
+(4, "2017-09-15", "Daniel", "Gallego Largo", 5, 14),
+(2, "2010-09-01", "Luis", "Martin Largo", 2, 14),
+(3, "2017-09-15", "Javier", "Moreno Heredia", 5, 15),
+(4, "2019-01-04", "Antonio", "Peña Colmenajero", 5, 15),
+(2, "2017-09-15", "Jorge", "Torreadrado Boal", 3, 16),
+(2, "2019-01-04", "David", "Delgado Buitrago", 4, 16),
+(2, "2013-10-13", "Edwin", "Polo Pina", 5, 17),
+(2, "2017-09-15", "Jofre", "Martinez Iturriaga", 5, 17),
+(4, "2013-10-13", "Luisa", "Urkia Odriozola", 6, 18),
+(3, "2023-09-15", "Isabel", "Villanueva Castro", 5, 18),
+(3, "2020-09-15", "Anibal", "Llamas Salas", 6, 19),
+(2, "2019-09-15", "Lucas", "Diment Waingorpin", 6, 19),
+(3, "2013-09-15", "Owen", "Mata Caseres", 4, 20),
+(3, "2016-09-15", "Dario", "Heras Fraga", 6, 20),
+(2, "2017-09-15", "Miquel", "Navarro Gonzalez", 2, 30);
