a4a69bd7f0044abc84d2dcefb0ea5b1d6bbacaab
Fraile2 <145047146+Fraile2@users.noreply.github.com>
2024-03-18 13:22:04 +0100

    Update consulta2.sql

--- Preguntas/consultasJoin/consulta2.sql
+++ Preguntas/consultasJoin/consulta2.sql
@@ -1,3 +1,8 @@
--- 
+-- Ej1 - Cuantos pilotos pilotan aviones de la marca boeing
+-- Entre el año 2013 y 2015 y hayan pilotado un martes.
 
-SELECT
+
+SELECT p1.nombre
+FROM piloto p1
+JOIN pilota p2
+ON p1.id_piloto=p2.piloto;
