ba106ae1bbbd19fa1c0a639b9f3dc6da5ec3424f
Fraile2 <145047146+Fraile2@users.noreply.github.com>
2024-04-04 20:57:28 +0200

    Update preguntas.sql

--- Preguntas/consultas_joins/preguntas.sql
+++ Preguntas/consultas_joins/preguntas.sql
@@ -1,10 +1,10 @@
 /*
 	Pregunta 1: Fácil
-	Muestra los DNI de los clientes que acaben con la letra 
-	Q y el DNI de los viajeros cuyo DNI acaben por D.
+	"Muestra los DNIs terminados en 'Q' de los clientes que hayan comprado el billete, así como los DNIs terminados en 'D' de los viajeros respectivamente."
+
 */
 
-SELECT c.dni_cliente, v.dni_viajero, b.numero_asiento
+SELECT c.dni_cliente, v.dni_viajero
 FROM viajero v
 JOIN billete b
 ON v.billete=b.id_billete
@@ -36,7 +36,7 @@ GROUP BY fecha_hora_vuelo
 HAVING marcas LIKE '%,%';
 
 /*	Pregunta 2
-	Pregunta 3: media 
+	Pregunta 4: media 
 	Muestra los nombres de los pilotos 
 	que empiecen por 'M' cuya aerolinea 
 	no sea 'Iberia', contenga una 'r'
@@ -51,4 +51,29 @@ ON p1.id_piloto=p2.piloto
 WHERE a.nombre<>'Iberia'
 AND a.nombre LIKE '%r%'
 AND TIMESTAMPDIFF(YEAR, p1.antiguedad, CURDATE())>8;
+
+/* Pregunta 4
+
+*/
+
+SELECT a.marca, count(a.modelo) As n_modelos
+FROM avion a
+JOIN pilota p
+ON a.matricula=p.avion
+JOIN vuelo v
+ON p.vuelo=v.id_vuelo
+JOIN puerta p2
+ON v.id_vuelo=p2.vuelo
+WHERE HOUR(fecha_hora_vuelo)=8
+GROUP BY a.marca;
+
+/** Pregunta 5 Facil
+	Cuantos tipos de billetes hay
+	sin usar limit
+*/
+SELECT tipo AS NombreBilletes
+FROM billete
+UNION
+SELECT tipo 
+FROM billete;
 
